0b06135d29e47c9be2ff7dc050a887806ececdc1
AngularMast <sanchzxel@gmail.com>
2019-08-09 00:20:16 +0800

    code review

--- src/main.js
+++ src/main.js
@@ -1,37 +1,31 @@
 import Vue from "vue";
 import VueAzureUploader from 'vue-azure-blob-upload'
+import Axios from "axios";
+import Datetime from 'vue-datetime'
+import VuetifyGoogleAutocomplete from 'vuetify-google-autocomplete';
 
 import App from "./App.vue";
 import router from "./router/";
 import store from "./store/";
-import Axios from "axios";
-
 import AuthPlugin from "./plugins/auth";
-
-
-Vue.use(AuthPlugin);
-import VuetifyGoogleAutocomplete from 'vuetify-google-autocomplete';
 import "./registerServiceWorker";
 import "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "font-awesome/css/font-awesome.css";
-
-import Datetime from 'vue-datetime'
-// You need a specific loader for CSS files
 import 'vue-datetime/dist/vue-datetime.css'
+
+// You need a specific loader for CSS files
  
+Vue.use(AuthPlugin);
 Vue.use(Datetime)
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = Axios;
 const token = localStorage.getItem("token");
+
 if (token) {
   Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
 }
-
-// Vue.prototype.$http.defaults.headers.common["Accept"] = "application/json";
-// Vue.prototype.$http.defaults.headers.common["Content-Type"] =
-//   "application/x-www-form-urlencoded";
 
 Vue.use(VuetifyGoogleAutocomplete, {
   apiKey: 'AIzaSyCpJOWuQiXHTAnmdxgqjPRCwOKkTllFtsg',
@@ -44,6 +38,5 @@ new Vue({
   store,
   render: h => h(App),
   created() {
-    //ctm-api
   }
 }).$mount("#app");
